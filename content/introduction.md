e4dcec40ee5f1b51113a17387ae29945367f3f6e
Aah Ahmad Kusumah <ahmad.kusumah@gmail.com>
2018-05-18 15:23:34 +0700

    initial comit

---
+++ content/introduction.md
@@ -0,0 +1,8 @@
+Title: Introduction
+Date: 2018-05-18 15:00
+Category: General
+Tags: introduction, migration
+Authors: A. Ahmad Kusumah
+Summary: This should be a future replacement or complementary for kusumah.wordpress.com
+
+This should be a future replacement if complementary blog for kusumah.worpdress.com
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'A. Ahmad Kusumah'
+SITENAME = 'Stacked Knowledge'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Jakarta'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Aah Ahmad Kusumah', 'https://kusumah.wordpress.com'),)
+
+# Social widget
+SOCIAL = (('Facebook', 'https://facebook.com/ahmad.kusumah'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
